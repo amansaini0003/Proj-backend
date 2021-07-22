5470eb8e531264ad1a449f1f74209fbdc971af6b
Aman Saini <amansaini0003@gmail.com>
2021-07-22 19:21:43 +0530

    initial commit

---
+++ models/user.js
@@ -0,0 +1,63 @@
+  const mongoose = require('mongoose');
+
+  const userSchema = new mongoose.Schema({
+        name: {
+            type: String,
+            required: true,
+            maxlength: 32,
+            trim: true
+        },
+        lastname: {
+            type: String,
+            maxlength: 32,
+            trim: true
+        },
+        email: {
+            type: String,
+            trim: true,
+            required: true,
+            unique: true
+        },
+        userinfo: {
+            type: String,
+            trim: true
+        },
+
+        //TODO: come back here
+
+        encry_password: {
+            type: String,
+            required: true
+        },
+
+        salt: String,
+        role: {
+            type: Number,
+            default: 0
+        },
+        purchases: {
+            type: Array,
+            default: []
+        }
+  });
+
+
+    userSchema.method = {
+        securePassword: function(plainpassword){
+            if (!password) return "";
+            
+            try {
+                return crypto.createHmac('sha256', this.salt)
+                       .update(plainpassword)
+                       .digest('hex');
+            } catch (err) {
+                return "";
+            }
+          
+        }
+
+    }
+
+
+
+module.exports = mongoose.model("User", userSchema);
