b71a628b1634a0401fcd9ca6419478fc7101abac
Aman Saini <amansaini0003@gmail.com>
2021-07-22 20:14:21 +0530

    user schema completed

--- models/user.js
+++ models/user.js
@@ -1,63 +1,78 @@
   const mongoose = require('mongoose');
+  const crypto = require('crypto');
+  const uuidv1 = require('uuid/v1');
 
   const userSchema = new mongoose.Schema({
-        name: {
-            type: String,
-            required: true,
-            maxlength: 32,
-            trim: true
-        },
-        lastname: {
-            type: String,
-            maxlength: 32,
-            trim: true
-        },
-        email: {
-            type: String,
-            trim: true,
-            required: true,
-            unique: true
-        },
-        userinfo: {
-            type: String,
-            trim: true
-        },
+      name: {
+          type: String,
+          required: true,
+          maxlength: 32,
+          trim: true
+      },
+      lastname: {
+          type: String,
+          maxlength: 32,
+          trim: true
+      },
+      email: {
+          type: String,
+          trim: true,
+          required: true,
+          unique: true
+      },
+      userinfo: {
+          type: String,
+          trim: true
+      },
 
-        //TODO: come back here
+      encry_password: {
+          type: String,
+          required: true
+      },
 
-        encry_password: {
-            type: String,
-            required: true
-        },
-
-        salt: String,
-        role: {
-            type: Number,
-            default: 0
-        },
-        purchases: {
-            type: Array,
-            default: []
-        }
+      salt: String,
+      role: {
+          type: Number,
+          default: 0
+      },
+      purchases: {
+          type: Array,
+          default: []
+      }
   });
 
 
-    userSchema.method = {
-        securePassword: function(plainpassword){
-            if (!plainpassword) return "";
-            
-            try {
-                return crypto.createHmac('sha256', this.salt)
-                       .update(plainpassword)
-                       .digest('hex');
-            } catch (err) {
-                return "";
-            }
-          
-        }
-
-    }
+  userSchema.virtual("password")
+      .set(function (password) {
+          this._password = password;
+          this.salt = uuidv1();
+          this.encry_password = this.securePassword(password);
+      })
+      .get(function () {
+          return this._password;
+      })
 
 
+  userSchema.method = {
 
-module.exports = mongoose.model("User", userSchema);
+      authenticate: function (plainpassword) {
+          return this.securePassword(plainpassword) === this.encry_password;
+      },
+
+      securePassword: function (plainpassword) {
+          if (!plainpassword) return "";
+
+          try {
+              return crypto.createHmac('sha256', this.salt)
+                  .update(plainpassword)
+                  .digest('hex');
+          } catch (err) {
+              return "";
+          }
+      }
+
+  }
+
+
+
+  module.exports = mongoose.model("User", userSchema);
