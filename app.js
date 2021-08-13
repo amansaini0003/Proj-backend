90f7474e422020ff7d9dcf9465b075ff5bd93237
Aman Saini <amansaini0003@gmail.com>
2021-08-13 20:04:00 +0530

    database connectivity

---
+++ app.js
@@ -0,0 +1,25 @@
+require("dotenv").config();
+
+const mongoose = require("mongoose");
+const express = require("express");
+
+const app = express();
+
+mongoose
+  .connect(process.env.DATABASE, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+  })
+  .then(() => {
+    console.log("DB CONNECTED");
+  })
+  .catch(() => {
+    console.log("DB GOT OOPS...");
+  });
+
+const port = process.env.PORT || 8000;
+
+app.listen(port, () => {
+  console.log(`App is running at port ${port}...`);
+});
