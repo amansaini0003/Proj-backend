b304905f3317d6f5967b4a649f3641e645e99981
Aman Saini <amansaini0003@gmail.com>
2021-08-16 19:25:40 +0530

    setting validation in routes

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,6 +1,16 @@
 const User = require("../models/user");
+const { check, validationResult } = require("express-validator");
 
 exports.signup = (req, res) => {
+  
+  const errors = validationResult(req);
+  if(!errors.isEmpty()){
+    return res.status(422).json({
+        errors: errors.array()
+    });
+  }
+  
+  
   const user = new User(req.body);
   user.save((err, user) => {
     if (err) {
--- routes/auth.js
+++ routes/auth.js
@@ -1,8 +1,21 @@
 var express = require("express");
+const { check, validationResult } = require("express-validator");
 var router = express.Router();
 const { signout, signup } = require("../controllers/auth");
 
-router.post("/signup", signup);
+router.post(
+  "/signup",
+  [
+    check("name")
+      .isLength({ min: 3 })
+      .withMessage("name must be atleast 3 chars long"),
+    check("email").isEmail().withMessage("email is required"),
+    check("password")
+      .isLength({ min: 8 })
+      .withMessage("password must be atleast 8 chars long"),
+  ],
+  signup
+);
 router.get("/signout", signout);
 
 module.exports = router;
